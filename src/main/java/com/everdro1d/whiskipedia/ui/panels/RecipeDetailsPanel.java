0c1ef6ddd7e7a3ec157128f93e4ac28fc2ce2d7c
everdro1d <everdro1d-github@pm.me>
2026-06-21 13:44:29 -0600

    feat: add most core components to RecipeDetailsPanel (title, desc., ingr., instr.)

--- src/main/java/com/everdro1d/whiskipedia/ui/panels/RecipeDetailsPanel.java
+++ src/main/java/com/everdro1d/whiskipedia/ui/panels/RecipeDetailsPanel.java
@@ -1,5 +1,9 @@
 package com.everdro1d.whiskipedia.ui.panels;
 
+import com.everdro1d.whiskipedia.core.RecipeObject;
+import com.everdro1d.whiskipedia.core.RecipeWorker;
+import com.everdro1d.whiskipedia.ui.MainWindow;
+
 import javax.swing.*;
 
 import java.awt.*;
@@ -12,9 +16,20 @@ public class RecipeDetailsPanel extends JPanel {
     // TODO populate recipe panel contents from recipe object
     // TODO update upon selecting a new object from the list
 
+    private JLabel recipeTitleLabel;
+    private JTextArea descriptionDisplayArea;
+    private JSplitPane ingredientInstructionsContainer;
+        private JScrollPane ingredientsScrollPane;
+            private DefaultListModel<RecipeObject.Ingredient> ingredientsModel;
+            private JList<RecipeObject.Ingredient> ingredientsList;
+        private JScrollPane instructionsScrollPane;
+            private JTextArea instructionsDisplayArea;
 
     // UI Text Defaults ---
     private String recipeDetailsTitleText = "Recipe Details";
+    private String descriptionAreaTitleText = "Description";
+    private String ingredientsPaneTitleText = "Ingredients List";
+    private String instructionsPaneTitleText = "Instructions";
 
     // Other ---
     private final int MIN_PANEL_WIDTH = 515;
@@ -35,6 +50,9 @@ public class RecipeDetailsPanel extends JPanel {
     private void addComponentToLocale() {
         Map<String, String> map = new TreeMap<>();
         map.put("recipeDetailsTitleText", recipeDetailsTitleText);
+        map.put("ingredientsPaneTitleText", ingredientsPaneTitleText);
+        map.put("instructionsPaneTitleText", instructionsPaneTitleText);
+        map.put("descriptionAreaTitleText", descriptionAreaTitleText);
 
 
         if (!localeManager.getClassesInLocaleMap().contains("MainWindow")) {
@@ -47,6 +65,10 @@ public class RecipeDetailsPanel extends JPanel {
     private void useLocale() {
         Map<String, String> varMap = localeManager.getComponentSpecificMap("MainWindow", "RecipeDetailsPanel");
         recipeDetailsTitleText = varMap.getOrDefault("recipeDetailsTitleText", recipeDetailsTitleText);
+        ingredientsPaneTitleText = varMap.getOrDefault("ingredientsPaneTitleText", ingredientsPaneTitleText);
+        instructionsPaneTitleText = varMap.getOrDefault("instructionsPaneTitleText", instructionsPaneTitleText);
+        descriptionAreaTitleText = varMap.getOrDefault("descriptionAreaTitleText", descriptionAreaTitleText);
+
     }
 
     private void initializeGUIComponents() {
@@ -56,11 +78,82 @@ public class RecipeDetailsPanel extends JPanel {
         this.setLayout(new GridBagLayout());
         if (guiDebugColoring) this.setBackground(Color.BLUE);
         {
-            /* TODO
-             * # Title
-             * > Description
-             * | Ingredients | Instructions |
-             */
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            c.anchor = GridBagConstraints.FIRST_LINE_START;
+            c.fill = GridBagConstraints.BOTH;
+
+            recipeTitleLabel = new JLabel("Placeholder"); //TODO add edit button in right hand corner (icon)
+            recipeTitleLabel.setFont(new Font(MainWindow.fontName, Font.BOLD, MainWindow.fontSize + 10));
+            recipeTitleLabel.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+            if (guiDebugColoring) recipeTitleLabel.setBackground(Color.RED);
+            this.add(recipeTitleLabel, c);
+
+            c.gridy++;
+
+            descriptionDisplayArea = new JTextArea("Placeholder");
+            descriptionDisplayArea.setBorder(BorderFactory.createTitledBorder(descriptionAreaTitleText));
+            descriptionDisplayArea.setEditable(false);
+            descriptionDisplayArea.setFocusable(false);
+            descriptionDisplayArea.setLineWrap(true);
+            descriptionDisplayArea.setWrapStyleWord(true);
+            descriptionDisplayArea.setFont(MainWindow.FONT);
+            if (guiDebugColoring) descriptionDisplayArea.setBackground(Color.PINK);
+            this.add(descriptionDisplayArea, c);
+
+            c.gridy++;
+            c.weighty = 1;
+
+            ingredientInstructionsContainer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+            if (guiDebugColoring) ingredientInstructionsContainer.setBackground(Color.ORANGE);
+            this.add(ingredientInstructionsContainer, c);
+            {
+                int min_width = (int)(MIN_PANEL_WIDTH/2.5);
+                int min_height = MIN_PANEL_HEIGHT/3;
+
+                // --- Ingredients ---
+                ingredientsModel = new DefaultListModel<>();
+
+                ingredientsList = new JList<>(ingredientsModel);
+                ingredientsList.setFont(MainWindow.FONT);
+                if (guiDebugColoring) ingredientsList.setBackground(Color.YELLOW);
+
+                ingredientsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+                ingredientsList.setFocusable(false);
+                ingredientsList.setLayoutOrientation(JList.VERTICAL_WRAP);
+
+                ingredientsScrollPane = new JScrollPane(ingredientsList);
+                ingredientsScrollPane.setBorder(BorderFactory.createTitledBorder(ingredientsPaneTitleText));
+                ingredientsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                ingredientsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+                ingredientsScrollPane.setMinimumSize(new Dimension(min_width, min_height));
+                ingredientInstructionsContainer.setLeftComponent(ingredientsScrollPane);
+
+                // --- Instructions ---
+                instructionsScrollPane = new JScrollPane();
+                instructionsScrollPane.setBorder(BorderFactory.createTitledBorder(instructionsPaneTitleText));
+                instructionsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+                instructionsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+                instructionsScrollPane.setMinimumSize(new Dimension(min_width, min_height));
+                {
+
+                    instructionsDisplayArea = new JTextArea();
+                    instructionsDisplayArea.setEditable(false);
+                    instructionsDisplayArea.setFocusable(false);
+                    instructionsDisplayArea.setLineWrap(true);
+                    instructionsDisplayArea.setWrapStyleWord(true);
+                    instructionsDisplayArea.setFont(MainWindow.FONT);
+                    if (guiDebugColoring) instructionsDisplayArea.setBackground(Color.GREEN);
+                    instructionsScrollPane.setViewportView(instructionsDisplayArea);
+                }
+                ingredientInstructionsContainer.setRightComponent(instructionsScrollPane);
+            }
+
+            c.weighty = 0;
         }
     }
 }
